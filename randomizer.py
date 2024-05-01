2ee4645c1b530a30d489896368899d3746be066c
Jonathan Tan <jontan_@hotmail.com>
2024-05-01 12:29:59 -0400

    Update Savage Labyrinth and octo fairy hints for AP

--- randomizer.py
+++ randomizer.py
@@ -229,7 +229,7 @@ class WWRandomizer:
       self.enemies,
       self.palettes,
       self.items,
-      # self.hints,
+      self.hints,
     ]
     
     self.logic.initialize_from_randomizer_state()
