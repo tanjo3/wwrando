c20509cf0ee9fe7c56319ecade3f623b71f45dc3
Jonathan Tan <jontan_@hotmail.com>
2024-08-15 12:51:05 -0400

    Split magic item get func into two funcs

--- tweaks.py
+++ tweaks.py
@@ -294,9 +294,14 @@ def make_items_progressive(self: WWRandomizer):
     picto_box_item_get_func_addr = item_get_funcs_list + picto_box_item_id*4
     self.dol.write_data(fs.write_u32, picto_box_item_get_func_addr, self.main_custom_symbols["progressive_picto_box_item_func"])
   
+  # To make magic meter work for AP, use two different item get funcs for magic.
+  # The AP client will handle which one is called.
   for magic_meter_item_id in [0xB1, 0xB2]:
     magic_meter_item_get_func_addr = item_get_funcs_list + magic_meter_item_id*4
-    self.dol.write_data(fs.write_u32, magic_meter_item_get_func_addr, self.main_custom_symbols["progressive_magic_meter_item_func"])
+    if magic_meter_item_id == 0xB1:
+      self.dol.write_data(fs.write_u32, magic_meter_item_get_func_addr, self.main_custom_symbols["magic_meter_item_func"])
+    else:
+      self.dol.write_data(fs.write_u32, magic_meter_item_get_func_addr, self.main_custom_symbols["magic_meter_upgrade_item_func"])
   
   # Register which item ID is for which progressive item.
   self.register_renamed_item(0x38, "Progressive Sword")
