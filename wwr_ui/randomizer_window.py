9de1cf5d39a8f9cd6c56f5f5078ae136a5f15d07
Jonathan Tan <jtan97@uwo.ca>
2026-04-02 14:09:16 -0400

    Create new UI tab and filterable lists for tricks

--- wwr_ui/randomizer_window.py
+++ wwr_ui/randomizer_window.py
@@ -17,6 +17,7 @@ from ruamel.yaml.error import YAMLError
 yaml_dumper = YAML(typ="rt") # Use RoundTripDumper for pretty-formatted dumps.
 
 from options.wwrando_options import Options, SwordMode
+from logic.tricks import ALL_TRICK_NAMES, ALL_TRICKS
 from randomizer import WWRandomizer, TooFewProgressionLocationsError, InvalidCleanISOError, PermalinkWrongVersionError, PermalinkWrongCommitError
 from version import VERSION
 from wwrando_paths import SETTINGS_PATH, ASSETS_PATH, IS_RUNNING_FROM_SOURCE, RANDO_ROOT_PATH
@@ -58,6 +59,33 @@ class WWRandomizerWindow(QMainWindow):
     self.starting_gear_model = QStringListModel()
     self.starting_gear_model.setStringList(DEFAULT_STARTING_ITEMS.copy())
     self.ui.starting_gear.setModel(self.starting_gear_model)
+    
+    self.trick_descriptions = {trick.name: trick.description for trick in ALL_TRICKS}
+    
+    self.ui.enable_trick.clicked.connect(self.enable_selected_tricks)
+    self.available_tricks_model = QStringListModel()
+    self.available_tricks_model.setStringList(list(ALL_TRICK_NAMES))
+    self.filtered_available_tricks = QSortFilterProxyModel()
+    self.filtered_available_tricks.setSourceModel(self.available_tricks_model)
+    self.filtered_available_tricks.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+    self.ui.available_tricks.setModel(self.filtered_available_tricks)
+    self.ui.filter_available_tricks.textChanged.connect(self.filtered_available_tricks.setFilterFixedString)
+    
+    self.ui.disable_trick.clicked.connect(self.disable_selected_tricks)
+    self.enabled_tricks_model = QStringListModel()
+    self.enabled_tricks_model.setStringList([])
+    self.filtered_enabled_tricks = QSortFilterProxyModel()
+    self.filtered_enabled_tricks.setSourceModel(self.enabled_tricks_model)
+    self.filtered_enabled_tricks.setFilterCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+    self.ui.enabled_tricks.setModel(self.filtered_enabled_tricks)
+    self.ui.filter_enabled_tricks.textChanged.connect(self.filtered_enabled_tricks.setFilterFixedString)
+    
+    self.ui.available_tricks.setMouseTracking(True)
+    self.ui.available_tricks.entered.connect(self.show_trick_description)
+    self.ui.available_tricks.installEventFilter(self)
+    self.ui.enabled_tricks.setMouseTracking(True)
+    self.ui.enabled_tricks.entered.connect(self.show_trick_description)
+    self.ui.enabled_tricks.installEventFilter(self)
     
     # We use an Options instance to represent the defaults instead of directly accessing each options default so that
     # default_factory works correctly.
@@ -162,10 +190,13 @@ class WWRandomizerWindow(QMainWindow):
     selection = source.selectionModel().selectedIndexes()
     # Remove starting from the last so the previous indices remain valid
     selection.sort(reverse = True, key = lambda x: x.row())
+    dest_model = dest.model()
+    if isinstance(dest_model, QSortFilterProxyModel):
+      dest_model = dest_model.sourceModel()
     for item in selection:
       value = item.data()
       source.model().removeRow(item.row())
-      self.append_row(dest.model(), value)
+      self.append_row(dest_model, value)
   
   def add_to_starting_gear(self):
     self.move_selected_rows(self.ui.randomized_gear, self.ui.starting_gear)
@@ -176,6 +207,21 @@ class WWRandomizerWindow(QMainWindow):
     self.move_selected_rows(self.ui.starting_gear, self.ui.randomized_gear)
     self.ui.randomized_gear.model().sourceModel().sort(0)
     self.update_settings()
+  
+  def enable_selected_tricks(self):
+    self.move_selected_rows(self.ui.available_tricks, self.ui.enabled_tricks)
+    self.enabled_tricks_model.sort(0)
+    self.update_settings()
+  
+  def disable_selected_tricks(self):
+    self.move_selected_rows(self.ui.enabled_tricks, self.ui.available_tricks)
+    self.available_tricks_model.sort(0)
+    self.update_settings()
+  
+  def show_trick_description(self, index):
+    trick_name = index.data()
+    if trick_name and trick_name in self.trick_descriptions:
+      self.set_option_description(self.trick_descriptions[trick_name])
   
   def update_health_label(self):
     pohs = self.ui.starting_pohs.value()
@@ -516,6 +562,14 @@ class WWRandomizerWindow(QMainWindow):
       return None
   
   def eventFilter(self, target: QObject, event: QEvent):
+    if target in (self.ui.available_tricks, self.ui.enabled_tricks):
+      # Trick list views use the entered signal for per-item descriptions.
+      # Only handle Leave to clear the description.
+      if event.type() == QEvent.Type.Leave:
+        self.set_option_description(None)
+        return True
+      return QMainWindow.eventFilter(self, target, event)
+    
     if event.type() == QEvent.Type.Enter:
       option = self.get_option_from_widget(target)
       if option:
@@ -567,7 +621,7 @@ class WWRandomizerWindow(QMainWindow):
       if widget.model() is None:
         return []
       model = widget.model()
-      if isinstance(model, ModelFilterOut):
+      if isinstance(model, (ModelFilterOut, QSortFilterProxyModel)):
         model = model.sourceModel()
       model.sort(0)
       return [model.data(model.index(i, 0), Qt.ItemDataRole.DisplayRole) for i in range(model.rowCount())]
@@ -622,6 +676,12 @@ class WWRandomizerWindow(QMainWindow):
           model = model.sourceModel()
         model.setStringList(new_value)
         model.sort(0)
+      
+      if option_name == "enabled_tricks":
+        enabled_set = set(new_value)
+        available = sorted([t for t in ALL_TRICK_NAMES if t not in enabled_set])
+        self.available_tricks_model.setStringList(available)
+        self.available_tricks_model.sort(0)
     else:
       print("Option widget is invalid: %s" % option_name)
   
